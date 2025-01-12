04adf13a544c08e882dcaee4c7adfaab2baad559
abhijeet1999 <abhijeetaherungottil@gmail.com>
2025-01-12 13:18:02 -0500

    Initial Setup

---
+++ GHFollower/GHFollower/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GHFollower
+//
+//  Created by Abhijeet Cherungottil on 1/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemRed
+    }
+
+
+}
+
