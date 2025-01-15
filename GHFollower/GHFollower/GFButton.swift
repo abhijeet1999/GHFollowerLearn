4b7eddec9cfac7f36b2d01d5e8819311103cd8d0
abhijeet1999 <abhijeetaherungottil@gmail.com>
2025-01-14 20:25:57 -0500

    Custom UITextfield and Uibutton added

---
+++ GHFollower/GHFollower/GFButton.swift
@@ -0,0 +1,37 @@
+//
+//  GFButton.swift
+//  GHFollower
+//
+//  Created by Abhijeet Cherungottil on 1/14/25.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(background: UIColor, title:String){
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        
+        layer.cornerRadius = 10;
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+        
+    }
+    
+}
